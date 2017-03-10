9b758b3547ebfbd638ade755d3787f915e60013e
Ryan Spencer (RIT Student) <rxs2833@ad.rit.edu>
2017-03-09 23:46:33 -0500

    Added rooms start

--- src/server.js
+++ src/server.js
@@ -1,15 +1,30 @@
 const http = require('http');
 const fs = require('fs');
+var url = require('url');
+var query = require('querystring');
 const socketio = require('socket.io');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const rooms = fs.readFileSync(`${__dirname}/../client/rooms.html`);
 
 const onRequest = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
-  response.end(0);
+    let parsedURL = url.parse(request.url);
+    let params = query.parse(parsedURL.query);
+    console.dir(parsedURL.pathname);
+    console.dir(params);
+  
+    if (parsedURL.pathname === "/game") {
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.write(index);
+        repsonse.end(0);
+    }
+    else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(rooms);
+        response.end(0);
+    }
 };
 
 const app = http.createServer(onRequest).listen(port);
