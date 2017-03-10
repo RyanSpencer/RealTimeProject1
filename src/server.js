13875ed0f57202ebd549baa5e00a44092310c4a6
Ryan Spencer <rxs2833@g.rit.edu>
2017-03-10 00:56:53 -0500

    Finished I think cool cool

--- src/server.js
+++ src/server.js
@@ -1,30 +1,15 @@
 const http = require('http');
 const fs = require('fs');
-var url = require('url');
-var query = require('querystring');
 const socketio = require('socket.io');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-const rooms = fs.readFileSync(`${__dirname}/../client/rooms.html`);
 
 const onRequest = (request, response) => {
-    let parsedURL = url.parse(request.url);
-    let params = query.parse(parsedURL.query);
-    console.dir(parsedURL.pathname);
-    console.dir(params);
-  
-    if (parsedURL.pathname === "/game") {
-        response.writeHead(200, {'Content-Type' : 'text/html'});
-        response.write(index);
-        repsonse.end(0);
-    }
-    else {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(rooms);
-        response.end(0);
-    }
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end(0);
 };
 
 const app = http.createServer(onRequest).listen(port);
@@ -33,11 +18,18 @@ console.log(`Listening on 127.0.0.1 ${port}`);
 
 const io = socketio(app);
 
+const players = { one: 0, two: 0, three: 0, four: 0 };
+
 const onJoin = (sock) => {
   const socket = sock;
 
-  socket.on('join', () => {
-    socket.join('room1');
+  socket.on('join', (data) => {
+    console.log(data);
+    if (data.room === 'room1') players.one++;
+    else if (data.room === 'room2') players.two++;
+    else if (data.room === 'room3') players.three++;
+    else players.four++;
+    socket.join(data);
   });
 };
 
@@ -45,12 +37,13 @@ const onDraw = (sock) => {
   const socket = sock;
 
   socket.on('drawGif', (data) => {
-    io.sockets.in('room1').emit('displayGif', data);
+    io.sockets.in(data[0].room).emit('displayGif', data);
   });
 };
 
 io.sockets.on('connection', (socket) => {
   console.log('started');
+  socket.emit('players', players);
   onJoin(socket);
   onDraw(socket);
 });
